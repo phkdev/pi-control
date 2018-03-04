fd14fcfa8ec1d128c23696ddb516f778778b6253
Philip Kreismayr <philip.kreismayr@lemon42.com>
2018-03-04 20:51:40 +0100

    remove backward

--- shield/Stepper.py
+++ shield/Stepper.py
@@ -5,7 +5,4 @@ m1 = PiMotor.Stepper("STEPPER1")
 
 # Rotate Stepper 1 Contiously in forward/backward direction
 while True:
-    m1.forward(0.1,10)  # Delay and rotations
-    time.sleep(2)
-    m1.backward(0.1,10)
-    time.sleep(2)
+    m1.forward(0.02,10)  # Delay and rotations
